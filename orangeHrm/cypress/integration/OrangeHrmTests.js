af1d1b78417fb9d6a3284ee0bc1a6613dac8f7d0
Kozma Norbert <norbibea@gmail.com>
2020-09-29 11:36:06 +0200

    Adding test and page object

--- orangeHrm/cypress/integration/OrangeHrmTests.js
+++ orangeHrm/cypress/integration/OrangeHrmTests.js
@@ -13,14 +13,23 @@ describe ('Users Page', function() {
         const navigationBar = loginPage.clickLoginButton()     
         const userPage = navigationBar.navigateUsersPage()
         userPage.printAllusername()
-    }) 
+    })  
 
     it('Check My info', function() {
 
         const loginPage = new LoginPage()
         const navigationBar = loginPage.clickLoginButton()     
         const myInfoPage = navigationBar.navigateMyInfoPage()
-        
+        cy.log(Cypress.env('firstName'))
+        myInfoPage.getFirstNameField().should('have.value', Cypress.env('firstName'))
+        myInfoPage.getLastNameField().should('have.value', Cypress.env('lastName'))
+        myInfoPage.getMiddleNameField().should('have.value', Cypress.env('middleName'))
+        myInfoPage.getEmployeeIdField().should('have.value', Cypress.env('employeeId'))
+        myInfoPage.getEmpBirthdayField().should('have.value', Cypress.env('empBirthday'))
+        myInfoPage.getMartialStatusField().should('contain', Cypress.env('martialStatus'))
+        myInfoPage.getGenderField().should('contain', Cypress.env('gender'))
+        myInfoPage.getNationalityField().should('contain', Cypress.env('nationality'))
+        myInfoPage.getLicenseExpDateField().should('have.value', Cypress.env('licenseExpDate'))
     })
 
    })
--- orangeHrm/cypress/pageobjects/MyInfoPage.js
+++ orangeHrm/cypress/pageobjects/MyInfoPage.js
@@ -2,7 +2,7 @@ class MyInfoPage{
     constructor(){
         cy.get("div#personal_details_tab > h4", { timeout: 30000 }).should('be.visible')
         //firstName
-        cy.get("#firstName").should('be.visible').as('firstName')
+        cy.get("#firstName", { timeout: 30000 }).should('be.visible').as('firstName')
         //lastName
         cy.get("#lastName").should('be.visible').as('lastName')
         //middleName
@@ -19,6 +19,42 @@ class MyInfoPage{
         cy.get("div#nation_code_inputfileddiv > .select-wrapper").should('be.visible').as('nationality')
         //licenseExpDate
         cy.get("#emp_dri_lice_exp_date").should('be.visible').as('licenseExpDate')
+    }
+
+    getFirstNameField(){
+        return cy.get("@firstName")
+    }
+
+    getLastNameField(){
+        return cy.get("@lastName")
+    }
+
+    getMiddleNameField(){
+        return cy.get("@middleName")
+    }
+
+    getEmployeeIdField(){
+        return cy.get("@employeeId")
+    }
+
+    getEmpBirthdayField(){
+        return cy.get("@empBirthday")
+    }
+
+    getMartialStatusField(){
+        return cy.get("@martialStatus")
+    }
+
+    getGenderField(){
+        return cy.get("@gender")
+    }
+
+    getNationalityField(){
+        return cy.get("@nationality")
+    }
+
+    getLicenseExpDateField(){
+        return cy.get("@licenseExpDate")
     }
 
 }
