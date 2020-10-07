532b42fc844867372b1277828dbf4fd4221d586e
Kozma Norbert <norbibea@gmail.com>
2020-10-07 12:07:30 +0200

    Separate tests

--- webShop/cypress/integration/OrderTests.js
+++ webShop/cypress/integration/OrderTests.js
@@ -1,28 +1,12 @@
 import HomePage from '../pageObjects/HomePage'
 import NavigationBar from '../pageObjects/NavigationBar'
 
-describe ('Users Page', function() {
+describe ('Order Tests', function() {
     beforeEach(function(){
         cy.visit('/')
         cy.fixture('orderTestData').then(function (orderTestData) {
             this.orderTestData = orderTestData;
           })
-    })
-
-  
-    it('Create a screenshot about the element with the girl in the red dress', function() {   
-        const homePage = new HomePage
-        homePage.createScreenshotInFirstElement()
-    })
-
-    it("Search for 'blouse'", function() {   
-        const description = 'Short sleeved blouse with feminine draped sleeve detail.'
-
-        const navigationBar = new NavigationBar
-        navigationBar.getSearcField().type('blouse')
-        const searchPage = navigationBar.clickSearchButton()
-        const quickViewtDialogBox = searchPage.clickQuickViewButton()
-        quickViewtDialogBox.getDescriptionText().should('have.text', description)
     })
 
     it('Faded Short order test', function() {   
---
+++ webShop/cypress/integration/SearchAndScreenshotTests.js
@@ -0,0 +1,24 @@
+import HomePage from '../pageObjects/HomePage'
+import NavigationBar from '../pageObjects/NavigationBar'
+ 
+ describe ('Search and screenshot Tests', function() {
+    beforeEach(function(){
+        cy.visit('/')
+    })
+
+    it('Create a screenshot about the element with the girl in the red dress', function() {   
+        const homePage = new HomePage
+        homePage.createScreenshotInFirstElement()
+    })
+
+    it("Search for 'blouse'", function() {   
+        const description = 'Short sleeved blouse with feminine draped sleeve detail.a'
+
+        const navigationBar = new NavigationBar
+        navigationBar.getSearcField().type('blouse')
+        const searchPage = navigationBar.clickSearchButton()
+        const quickViewtDialogBox = searchPage.clickQuickViewButton()
+        quickViewtDialogBox.getDescriptionText().should('have.text', description)
+    })
+
+})    
